c17f472ec75cf175f421ac936abf860362ac8922
Nicholas Martin <n.martin@movura.co.uk>
2021-03-07 04:48:05 +0000

    minimal reproduction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,4 @@
-const path = require("path")
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-image",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },],
+  plugins: [],
 }
